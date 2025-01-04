68fc0e3d7b7b1989546809f48cabb0ec9a0b61e0
HoutKysopanha <houtkysopanha69@gmail.com>
2025-01-04 16:31:49 +0700

    First Pull from main

--- src/views/DashBaord.vue
+++ src/views/DashBaord.vue
@@ -3,9 +3,6 @@
       <h2>Welcome to the Dashboard fsfsfsfafs</h2>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium consequuntur praesentium consectetur quaerat, commodi optio nostrum laudantium quos voluptate? Impedit libero excepturi quam iure harum inventore est, sed cupiditate explicabo!</p>
       <p>Scroll to see more content here...</p>
-      <!-- <div v-for="i in 50" :key="i">
-        <p>Content item {{ i }}</p>
-      </div> -->
     </main>
   </template>
   
